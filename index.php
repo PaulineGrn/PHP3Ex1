d96786d0304b21d2ecb3b3b25a5610336415b826
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-03 14:58:40 +0100

    PHP3Ex1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$month=array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP3Ex1</title>
+  </head>
+  <body>
+  </body>
+</html>
