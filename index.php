c6826905e244cff034e3ae595295a35a253df589
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-05 09:23:02 +0100

    PHP3Ex1

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php
-$month=array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
- ?>
+// Déclaration d'une variable months et un array qui correspond à un tableau
+$months=array('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+// Autre méthode
+$days = array();
+$days[0] = 'Lundi';
+$days[1] = 'Mardi';
+$days[2] = 'Mercredi';
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>PHP3Ex1</title>
-  </head>
-  <body>
-  </body>
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP3Ex1</title>
+</head>
+<body>
+</body>
 </html>
